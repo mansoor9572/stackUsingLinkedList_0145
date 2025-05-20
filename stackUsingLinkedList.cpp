3040c233ef77b99defe8e6a4a60a4ca793c41eec
mansoor <t.alyamani95@gmail.com>
2025-05-20 11:10:32 +0700

    Traverse and print stack elements

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -52,4 +52,13 @@ class stack
             {
                 cout << "List is Empty." << endl;
             }
-           
+            else{
+                    Node *current = top;
+                    while (current != NULL)
+                    {
+                        cout << current->data << " " << endl;
+                        current = current->next;
+                    }
+                    cout << endl;
+            }
+        }
