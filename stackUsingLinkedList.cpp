155f0d80729ae6cac446830ac9d39639a0bbff53
mansoor <t.alyamani95@gmail.com>
2025-05-20 11:06:09 +0700

    Push value onto stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -13,3 +13,26 @@ class Node
         }
 };
 
+class stack
+{
+    private:
+        Node *top;
+
+    public:
+        stack()
+        {
+            top = NULL;
+        }
+
+        int push(int value)
+        {
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value: " << value << endl;
+            return value;
+
+        }
+
+      
