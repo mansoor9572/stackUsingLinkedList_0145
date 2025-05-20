71cf8c0f24004ab54716e4991eb30475773a1473
mansoor <t.alyamani95@gmail.com>
2025-05-20 11:23:00 +0700

    End of menu loop and program 😊

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -109,4 +109,8 @@ int main() {
             default:
                 cout << "Invalid choice. Try again." << endl;
                 break;
-      
+        }
+        cout << endl;
+    }
+    return 0;
+}
